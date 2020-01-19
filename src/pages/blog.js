765897efda2053633e4252a5de59c1bada848ce3
vasilijus <vasiliokas@gmail.com>
2020-01-19 18:02:56 +0000

    #11 getting posts , from .md files

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,38 @@
 import React from "react";
 import Layout from "../components/layout";
+import {  graphql, useStaticQuery } from 'gatsby';
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(data);
   return (
     <Layout>
       <h1>Blog</h1>
       <p>Blog page</p>
+
+
+        {data.allMarkdownRemark.edges.map( (edge) => {
+          return (
+            <li>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+     
     </Layout>
   );
 };
